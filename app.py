f180837b449fab6e27c570fe9da5fbf1a603f714
Rajarshi Nag <RajarshiNag@MacBook-Air.Dlink>
2024-03-06 01:41:21 +0530

    Other url added

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('About.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/products')
+def contact():
+    return render_template('products.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
